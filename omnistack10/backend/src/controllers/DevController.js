4c2c9229d97dae91ce745a9be8f921372a6494d0
Gabriel Coutinho <gabrielcoutinho20@gmail.com>
2020-01-18 23:07:25 -0300

    adicionado update back-end

--- omnistack10/backend/src/controllers/DevController.js
+++ omnistack10/backend/src/controllers/DevController.js
@@ -20,14 +20,14 @@ module.exports = {
             const response = await axios.get(`https://api.github.com/users/${github_user}`);
 
             const { name = login, avatar_url, bio } = response.data;
-    
+
             const techsArray = parseStringAsArray(techs);
-    
+
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude],
             }
-    
+
             dev = await Dev.create({
                 github_user,
                 name,
@@ -40,5 +40,26 @@ module.exports = {
 
 
         return res.json(dev);
+    },
+
+    async update(req, res) {
+        const { github_user } = req.params;
+        const { techs, ...rest } = req.body;
+        rest.github_user = github_user;
+
+        const dev = await Dev.findOne({ github_user });
+
+        if (techs)
+            var techsArray = parseStringAsArray(techs);
+
+        const newdev = await Dev.updateOne({ github_user }, {
+            techs: techs ? techsArray : dev.techs,
+            ...rest
+        });
+
+        return res.json({
+            modifiedCount: newdev.nModified,
+            ok: newdev.ok
+        });
     },
 };
--- omnistack10/backend/src/routes.js
+++ omnistack10/backend/src/routes.js
@@ -7,7 +7,7 @@ const routes = Router();
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
-
+routes.put('/devs/:github_user', DevController.update);
 routes.get('/search', SearchController.index);
 
 module.exports = routes;
